2e0a982fef2059240a9c83add7db492b36a06f60
andres ballares <andr3sb@hotmail.com>
2021-12-08 12:20:05 -0500

    added code to Home component

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Searchbar from "./Searchbar";
+import Video from "./Video";
+import VideoThumbNail from "./VideoThumbNail";
+import "./Home.css";
+
+
+class Home extends React.component {
+    constructor() {
+        super();
+        this.state = {
+            display: [],
+        }
+    }
+    render(){
+        return (
+            <section>
+                <Searchbar />
+                <Video />
+                <VideoThumbNail />
+            </section>
+        )
+    }
+}
+
+export default Home;
