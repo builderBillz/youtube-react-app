9ef617f86e5a2fc6810e5af1316318b3034fef35
Billy Dominique <billydominique@pursuit.org>
2021-12-08 12:39:05 -0500

    updating changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+      <>
+        null
+      </>
+    );
+  };
+
+  export default Home
