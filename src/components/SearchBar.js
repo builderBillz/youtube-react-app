c81ba6899193aa504db84569c6bfc048860dc683
andres ballares <andr3sb@hotmail.com>
2021-12-09 11:07:22 -0500

    updated SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,38 @@
+import React from "react";
 import "./SearchBar.css";
 
-const SearchBar = () => {
-    return (
-        <form>
-            <input type="text" placeholder="Search..." />
+class SearchBar extends React.component  {
+    constructor() {
+        super();
+        this.state = {
+            value: '',
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    handleUserInput = (event) => {
+        const { value } = event.target.value;
+        this.setState({
+            value: event.target.value,
+        })
+    }
+    
+        render() {
+            return ( 
+        
+        <form onSubmit={this.handleSubmit} className="video-container">
+            <input 
+            type="text" 
+            placeholder="Search..."
+            onChange={this.handleUserInput}
+            />
             <button type="submit">Search</button>
         </form>
-    )
+    );
+    }
 }
 
 export default SearchBar;
