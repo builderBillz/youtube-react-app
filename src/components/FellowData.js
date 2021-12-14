d044a859ed21552d795c08686f1e670ed8a1d1d4
Angie G <angieg@Angies-MBP.fios-router.home>
2021-12-14 08:53:27 -0500

    FellowData Updates

--- src/components/FellowData.js
+++ src/components/FellowData.js
@@ -1,26 +1,30 @@
 const FellowData = [
-    {
+    {   
+        id: '11102Melissa',
         image: 'https://i.imgur.com/cS0lAc4.jpg',
         name: 'Melissa Lopez',
         bio: 'My goal is to create apps, websites and video games. I love art, nature and music.',
         github: 'https://github.com/1zuzsuit492'
     },
     {
+        id: '11102Angie',
+        image: 'https://i.imgur.com/3yDGDnt.jpg',
+        name: 'Angie Guerrero',
+        bio: 'I am an Accessibility Software Engineer. I have fun painting and glassblowing.',
+        github: 'https://github.com/angie3j'
+    },
+    {
+        id: '11102Andres',
         image: 'https://i.imgur.com/modvN1U.jpg',
         name: 'Andres Ballares',
         bio: 'I enjoy front-end development. Ask me about Fitness Training and Performance Nutrition. Fun Fact: I am a Dog Lover.',
         github: 'https://github.com/AndresBallares'
     },
     {
-        image: 'https://i.imgur.com/3yDGDnt.jpg',
-        name: 'Angie Guerrero',
-        bio: 'I am an Accessibility Software Engineer. When I am not coding, I am either painting or glassblowing.',
-        github: 'https://github.com/angie3j'
-    },
-    {
+        id: '11102Billy',
         image: 'https://i.imgur.com/G4OEfyl.jpg',
         name: 'Billy Dominique',
-        bio: 'Some text that describes me lorem ipsum ipsum lorem.',
+        bio: 'I am a father and software developer who has always had a passion for technology. I like to motivate, laugh and write poetry.',
         github: 'https://github.com/builderBillz'
     }
     
